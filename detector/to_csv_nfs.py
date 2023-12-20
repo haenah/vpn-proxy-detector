ead3d8bcbe3924589ec76da31ad585390ed9fdcb
haenah <48505914+haenah@users.noreply.github.com>
2023-12-21 02:06:57 +0900

    Refactor

--- detector/to_csv_nfs.py
+++ detector/to_csv_nfs.py
@@ -1,7 +1,7 @@
-import numpy as np
 import pandas as pd
-import sys
 from nfstream import NFStreamer
+import os
+from nf_attributes import attributes
 
 from filenames import vpn_file_names, nvpn_file_names, file_name_groups
 
@@ -10,61 +10,22 @@ file_names = vpn_file_names + nvpn_file_names
 n_vpn = len(vpn_file_names)
 n_nvpn = len(nvpn_file_names)
 
-attributes = [
-    'expiration_id',
-    'src_ip',
-    'src_mac',
-    'src_oui',
-    'src_port',
-    'dst_ip',
-    'dst_mac',
-    'dst_oui',
-    'dst_port',
-    'protocol',
-    'ip_version',
-    'vlan_id',
-    'tunnel_id',
-    'bidirectional_first_seen_ms',
-    'bidirectional_last_seen_ms',
-    'bidirectional_duration_ms',
-    'bidirectional_packets',
-    'bidirectional_bytes',
-    'src2dst_first_seen_ms',
-    'src2dst_last_seen_ms',
-    'src2dst_duration_ms',
-    'src2dst_packets',
-    'src2dst_bytes',
-    'dst2src_first_seen_ms',
-    'dst2src_last_seen_ms',
-    'dst2src_duration_ms',
-    'dst2src_packets',
-    'dst2src_bytes',
-    'application_name',
-    'application_category_name',
-    'application_is_guessed',
-    'application_confidence',
-    'requested_server_name',
-    'client_fingerprint',
-    'server_fingerprint',
-    'user_agent',
-    'content_type'
-]
-
+os.makedirs("./preprocessed/nfs", exist_ok=True)
 
 for file_name, is_vpn in file_name_groups[-1]:
-    path = f"./{is_vpn}_pcap/{file_name}"
+    path = f"./data/{is_vpn}_pcap/{file_name}"
     streamer = NFStreamer(source=path, statistical_analysis=True)
 
     data_dict = {k: [] for k in attributes}
-    data_dict['label'] = []
-    data_dict['file_name'] = []
+    data_dict["label"] = []
+    data_dict["file_name"] = []
     for flow in streamer:
         for k in attributes:
             data_dict[k].append(getattr(flow, k))
-        data_dict['label'].append(is_vpn)
-        data_dict['file_name'].append(file_name)
+        data_dict["label"].append(is_vpn)
+        data_dict["file_name"].append(file_name)
 
     df = pd.DataFrame(data_dict)
-    df['src_freq'] = df.groupby('src_ip')['src_ip'].transform('count') / len(df)
+    df["src_freq"] = df.groupby("dst_ip")["dst_ip"].transform("count") / len(df)
     key = file_name.split("/")[-1].split(".")[0]
-    df.to_csv(f'./nfs/{key}.csv', mode='w')
+    df.to_csv(f"./preprocessed/nfs/{key}.csv", mode="w")
