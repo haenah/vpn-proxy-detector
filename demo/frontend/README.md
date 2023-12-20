9c0e481aceef5a1aab89bf62e51a52ce77b20cc8
sckimynwa <tsb03672@naver.com>
2023-12-20 09:30:19 +0900

    feat: initialize frontend platform

---
+++ demo/frontend/README.md
@@ -0,0 +1,7 @@
+# Streaming Platform Demo
+
+A video streaming demo platform to show if VPN Detector works properly.
+This platform provides video following the HLS protocol.
+
+## API
+The following api is being utilized: https://developers.themoviedb.org/3/getting-started/introduction
---
+++ demo/frontend/src/lib/mock/profile.ts
@@ -0,0 +1,17 @@
+export const MOCK_PROFILES: Record<string, User.Profile> = {
+  "1": {
+    uid: "1",
+    name: "Sangchul",
+    avatar: "/img/profile/1.png",
+  },
+  "2": {
+    uid: "2",
+    name: "Jaewon",
+    avatar: "/img/profile/2.png",
+  },
+  "3": {
+    uid: "3",
+    name: "Jiho",
+    avatar: "/img/profile/3.png",
+  },
+};
